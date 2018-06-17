c09e1c73fac6053e5ce2a316c6117cec6dfe0942
zouazano <sou.asano11@gmail.com>
2018-06-17 15:32:54 +0900

    yokudekita

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -6,4 +6,19 @@ class HousesController < ApplicationController
 	def show
 		@house = House.find(params[:id])
 	end
+
+	def new
+		@house = House.new
+	end
+
+	def create
+		@house = House.new(house_params)
+		@house.save
+	end
 end
+
+
+private
+	def house_params
+		params[:house].permit(:name, :strong_point, :prefecture_id, :zip_code, :address, :access, :hours, :shop_id, :price, :image, :type, :age, :madori, :land_area, :house_area, :built_time)
+	end
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,9 +1,4 @@
 class House < ApplicationRecord
 
-  rails_admin do
-    configure :player do
-      label 'Owner of this ball: '
-    end
-  end
 
 end
