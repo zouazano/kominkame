bfcf9f2bcc2a4ea5c4234efa71c72689c5478fdc
zouazano <sou.asano11@gmail.com>
2018-07-05 14:23:48 +0900

    torima

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,7 +32,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process resize_to_fill: [900, 600]
+     process resize_to_fill: [300, 200]
    end
 
    version :logo do
