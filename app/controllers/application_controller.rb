78a5b3e9eaccb95af3d336c284bdd7e560b3e80d
zouazano <sou.asano11@gmail.com>
2018-07-29 14:37:18 +0900

    railsadmin english

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
 
 	before_action :set_view_variant
 
+	before_action :set_locale
+ 
+  def set_locale
+    I18n.locale = :ja
+  end
+
 	private
 
   def set_view_variant
