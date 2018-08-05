5b0652804c6cbc3c91ddb2431b1fd23d61673d3e
zouazano <sou.asano11@gmail.com>
2018-08-05 11:47:47 +0900

    csv test

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@
 require "csv"
 
 CSV.foreach("db/seeds/rent_house.csv", headers: true) do |row|
-  attributes = row.to_h.slice("name")
+  attributes = row.to_h.slice("name", "strong_point", "prefecture_id", "rent", "maintenance_fee", "deposit", "gratuity_fee", "zip_code", "address", "access", "hours", "age", "madori", "land_area", "house_area", "built_time", "stayable", "rentable", "buyable", "recommendation", "shop_id", "source")
   RentHouse.where(name: row["name"]).first_or_create(attributes)
 end
