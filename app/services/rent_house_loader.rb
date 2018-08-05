c7de63abde0eaf7790715d2249ddd03e53847607
zouazano <sou.asano11@gmail.com>
2018-08-05 19:33:49 +0900

    rent_house_image url_ uploader made

--- app/services/rent_house_loader.rb
+++ app/services/rent_house_loader.rb
@@ -1,4 +1,5 @@
 require "csv"
+require "yaml"
 
 class RentHouseLoader
   def self.load
@@ -6,5 +7,16 @@ class RentHouseLoader
       attributes = row.to_h.slice("name", "strong_point", "prefecture_id", "rent", "maintenance_fee", "deposit", "gratuity_fee", "zip_code", "address", "access", "hours", "age", "madori", "land_area", "house_area", "built_time", "stayable", "rentable", "buyable", "recommendation", "shop_id", "source")
       RentHouse.where(name: row["name"]).first_or_create(attributes)
     end  
+  end
+
+  def self.yamload
+  	rh = YAML.load_file("db/seeds/rent_house.yml")
+    rh.each do |rent_house_data|
+    	rent_house = RentHouse.find_or_create_by(name: rent_house_data["name"])
+
+    	rent_house_data["image_url"].each do |image_url|
+    		rent_house.rent_house_images.find_or_create_by(remote_image_url: image_url)
+    	end
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require "csv"
-
-CSV.foreach("db/seeds/rent_house.csv", headers: true) do |row|
-  attributes = row.to_h.slice("name", "strong_point", "prefecture_id", "rent", "maintenance_fee", "deposit", "gratuity_fee", "zip_code", "address", "access", "hours", "age", "madori", "land_area", "house_area", "built_time", "stayable", "rentable", "buyable", "recommendation", "shop_id", "source")
-  RentHouse.where(name: row["name"]).first_or_create(attributes)
-end
