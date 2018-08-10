3577c2754e7b1d1816b41ad1c50f434753fc02c2
zouazano <sou.asano11@gmail.com>
2018-08-10 14:17:40 +0900

    torima

--- app/services/rent_house_loader.rb
+++ app/services/rent_house_loader.rb
@@ -5,7 +5,9 @@ class RentHouseLoader
   def self.load
     CSV.foreach("db/seeds/rent_house.csv", headers: true) do |row|
       attributes = row.to_h.slice("name", "strong_point", "prefecture_id", "rent", "maintenance_fee", "deposit", "gratuity_fee", "zip_code", "address", "access", "hours", "age", "madori", "land_area", "house_area", "built_time", "stayable", "rentable", "buyable", "recommendation", "shop_id", "source")
-      RentHouse.where(name: row["name"]).first_or_create(attributes)
+      rent_house = RentHouse.where(name: row["name"]).first_or_create(attributes)
+      p row["image_url1"]
+
     end  
   end
 
