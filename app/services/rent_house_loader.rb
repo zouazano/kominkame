3f969f26137df8daf742c8f58956e189e2df1590
zouazano <sou.asano11@gmail.com>
2018-08-10 14:47:15 +0900

    dragonfly loader made

--- app/services/rent_house_loader.rb
+++ app/services/rent_house_loader.rb
@@ -3,9 +3,25 @@ require "yaml"
 
 class RentHouseLoader
   def self.load
-    CSV.foreach("db/seeds/rent_house.csv", headers: true) do |row|
+    CSV.foreach("db/seeds/kominka.csv", headers: true) do |row|
       attributes = row.to_h.slice("name", "strong_point", "prefecture_id", "rent", "maintenance_fee", "deposit", "gratuity_fee", "zip_code", "address", "access", "hours", "age", "madori", "land_area", "house_area", "built_time", "stayable", "rentable", "buyable", "recommendation", "shop_id", "source")
-      RentHouse.where(name: row["name"]).first_or_create(attributes)
+      rent_house = RentHouse.where(name: row["name"]).first_or_create(attributes)
+      if row["image_url1"].present?
+        rent_house.rent_house_images.create(rent_house_image_url: row["image_url1"])
+      end
+      sleep 2
+      if row["image_url2"].present?
+        rent_house.rent_house_images.create(rent_house_image_url: row["image_url2"])
+      end
+      sleep 2
+      if row["image_url3"].present?
+        rent_house.rent_house_images.create(rent_house_image_url: row["image_url3"])
+      end
+      sleep 2
+      if row["image_url4"].present?
+        rent_house.rent_house_images.create(rent_house_image_url: row["image_url4"])
+      end
+      sleep 2
     end  
   end
 
