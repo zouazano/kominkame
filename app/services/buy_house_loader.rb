344884a1e81ba52ce2f25be6316e5a756c85f11f
zouazano <sou.asano11@gmail.com>
2018-08-12 12:52:10 +0900

    torima

--- app/services/buy_house_loader.rb
+++ app/services/buy_house_loader.rb
@@ -5,7 +5,7 @@ class BuyHouseLoader
   def self.load
     CSV.foreach("db/seeds/buy_house.csv", headers: true) do |row|
       attributes = row.to_h.slice("name", "strong_point", "prefecture_id", "price", "zip_code", "address", "access", "hours", "age", "madori", "land_area", "house_area", "built_time", "notes", "recommendation", "shop_id", "source", "image_url1", "image_url2", "image_url3", "image_url4")
-      buy_house = BuyHouse.where(name: row["name"]).first_or_create(attributes)
+      buy_house = BuyHouse.where(name: row["name"]).where(price: row["price"]).first_or_create(attributes)
       if row["image_url1"].present?
         buy_house.buy_house_images.create(buy_house_image_url: row["image_url1"])
       end
