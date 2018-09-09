21283c746aa2339282cc9c47907f937fd762efa4
zouazano <sou.asano11@gmail.com>
2018-09-09 18:02:36 +0900

    torima

--- app/controllers/buy_houses_controller.rb
+++ app/controllers/buy_houses_controller.rb
@@ -2,6 +2,5 @@ class BuyHousesController < ApplicationController
   def show
     @buy_house = BuyHouse.find(params[:id])
     buy_house_id = @buy_house.id
-    @prefecture = Prefecture.find(@buy_house.prefecture_id)
   end
 end
