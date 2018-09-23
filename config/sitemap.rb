52fe45f7a325559b110887be00a807834d5a2561
zouazano <sou.asano11@gmail.com>
2018-09-23 15:36:58 +0900

    spell mis

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -23,14 +23,14 @@ SitemapGenerator::Sitemap.create do
   #
   
   add root_path, :priority => 0.7, :changefreq => 'daily'
-  add buy__index_path
+  add buy_index_path
   add buy_prefectures_path
   Prefecture.find_each do |buy_prefecture|
     add buy_prefecture_path(buy_prefecture.id)
   end
 
 
-  
+
   BuyHouse.find_each do |buy_house|
     add buy_house_path(buy_house), :lastmod => buy_house.updated_at
   end
