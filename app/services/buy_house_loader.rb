b8c1fc155d0351c5cce4b19bf6054407f5df0e5d
zouazano <sou.asano11@gmail.com>
2018-10-07 20:00:55 +0900

    buy_house_loader fixed and buy_house added

--- app/services/buy_house_loader.rb
+++ app/services/buy_house_loader.rb
@@ -6,23 +6,31 @@ class BuyHouseLoader
     CSV.foreach("db/seeds/buy_house.csv", headers: true) do |row|
       attributes = row.to_h.slice("name", "strong_point", "price", "zip_code", "address", "access", "hours", "age", "madori", "land_area", "house_area", "built_time", "notes", "recommendation", "shop_id", "source", "image_url1", "image_url2", "image_url3", "image_url4", "built_date")
       attributes[:prefecture_id] = Prefecture.find_by(name: row["prefecture_name"]).id
-      buy_house = BuyHouse.where(name: row["name"]).where(price: row["price"]).first_or_create(attributes)
-      if row["image_url1"].present?
-        buy_house.buy_house_images.create(buy_house_image_url: row["image_url1"])
+      buy_house = BuyHouse.where(name: row["name"]).where(price: row["price"]).where(strong_point: row["strong_point"]).first
+      if buy_house.present?
+        next
+      else
+        buy_house = BuyHouse.new(attributes)
+        buy_house.save
       end
-      sleep 2
-      if row["image_url2"].present?
-        buy_house.buy_house_images.create(buy_house_image_url: row["image_url2"])
+      if buy_house.save
+        if row["image_url1"].present?
+          buy_house.buy_house_images.create(buy_house_image_url: row["image_url1"])
+        end
+        sleep 2
+        if row["image_url2"].present?
+          buy_house.buy_house_images.create(buy_house_image_url: row["image_url2"])
+        end
+        sleep 2
+        if row["image_url3"].present?
+          buy_house.buy_house_images.create(buy_house_image_url: row["image_url3"])
+        end
+        sleep 2
+        if row["image_url4"].present?
+          buy_house.buy_house_images.create(buy_house_image_url: row["image_url4"])
+        end
+        sleep 2
       end
-      sleep 2
-      if row["image_url3"].present?
-        buy_house.buy_house_images.create(buy_house_image_url: row["image_url3"])
-      end
-      sleep 2
-      if row["image_url4"].present?
-        buy_house.buy_house_images.create(buy_house_image_url: row["image_url4"])
-      end
-      sleep 2
       @buy_house = BuyHouse.all
       
       Rails.logger.debug buy_house.errors.inspect
