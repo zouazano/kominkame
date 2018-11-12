f01b57f012dcdb3ea3db39ff157f8a1320f48439
zouazano <sou.asano11@gmail.com>
2018-11-13 03:03:39 +0900

    torima

--- app/services/buy_house_crawler_suumo.rb
+++ app/services/buy_house_crawler_suumo.rb
@@ -5,8 +5,8 @@ require 'csv'
 
 class BuyHouseCrawlerSuumo
   def self.crawl
-    url = 'https://suumo.jp/b/kodate/kw/%E5%8F%A4%E6%B0%91%E5%AE%B6%E3%80%80%E7%89%A9%E4%BB%B6/?shubetsu=020&shubetsu=021&shubetsu=011&sort=9'
-
+    (2..5).each do |i|
+      uri = 'https://suumo.jp/b/kodate/kw/%E5%8F%A4%E6%B0%91%E5%AE%B6%E3%80%80%E7%89%A9%E4%BB%B6/#{i}/?sort=9&shubetsu=021'
     titles = []
     charset = nil
 
