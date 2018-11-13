6ad52f9c35c6c8e43fa172cc1d5b3c206f8210d6
zouazano <sou.asano11@gmail.com>
2018-11-13 15:02:06 +0900

    each removed from crawler

--- app/services/buy_house_crawler_suumo.rb
+++ app/services/buy_house_crawler_suumo.rb
@@ -5,8 +5,7 @@ require 'csv'
 
 class BuyHouseCrawlerSuumo
   def self.crawl
-    (2..5).each do |i|
-      uri = 'https://suumo.jp/b/kodate/kw/%E5%8F%A4%E6%B0%91%E5%AE%B6%E3%80%80%E7%89%A9%E4%BB%B6/#{i}/?sort=9&shubetsu=021'
+    url = 'https://suumo.jp/b/kodate/kw/%E5%8F%A4%E6%B0%91%E5%AE%B6%E3%80%80%E7%89%A9%E4%BB%B6/?sort=9&shubetsu=021'
     titles = []
     charset = nil
 
