f541099cc9dd7fa5073974fcba475d49887e297e
zouazano <sou.asano11@gmail.com>
2018-11-23 17:04:50 +0900

    crawler img igai

--- app/services/buy_house_crawler_suumo.rb
+++ app/services/buy_house_crawler_suumo.rb
@@ -37,7 +37,7 @@ class BuyHouseCrawlerSuumo
 
   def self.detail_crawl
 
-    kominka = ["https://suumo.jp/chukoikkodate/__JJ_JJ010FJ100_arz1040z2bsz1021z2ncz188893403.html?suit=nsuusbsp20121129001", "https://suumo.jp/chukoikkodate/__JJ_JJ010FJ100_arz1060z2bsz1021z2ncz190703347.html?suit=nsuusbsp20121129001"]
+    kominka = ["https://suumo.jp/chukoikkodate/__JJ_JJ010FJ100_arz1040z2bsz1021z2ncz190423197.html?suit=nsuusbsp20121129001", "https://suumo.jp/chukoikkodate/__JJ_JJ010FJ100_arz1060z2bsz1021z2ncz189283371.html?suit=nsuusbsp20121129001", "https://suumo.jp/chukoikkodate/__JJ_JJ010FJ100_arz1090z2bsz1021z2ncz189937175.html?suit=nsuusbsp20121129001"]
 
     CSV.open("buy_house_suumo.csv", "w") do |csv|
       header = ['name', 'address', 'access', 'madori', 'land_area', 'house_area', 'built_date', 'strong_point', 'prefecture_name', 'price', 'zip_code', 'hours', 'age', 'built_time', 'recommendation', 'notes', 'shop_id', 'source', 'image_url1', 'image_url2', 'image_url3', 'image_url4']
@@ -47,10 +47,14 @@ class BuyHouseCrawlerSuumo
         user_agent = "User-Agent: Mozilla/5.0 (Windows NT 6.1; rv:28.0) Gecko/20100101 Firefox/28.0"
         charset = nil
 
-
-        html = open(url) do |f|
-          charset = f.charset
-          f.read
+        begin
+          html = open(url) do |f|
+            charset = f.charset
+            f.read
+          end
+        rescue OpenURI::HTTPError
+          sleep 1
+          next
         end
 
         buy_house_info = []
@@ -58,10 +62,10 @@ class BuyHouseCrawlerSuumo
         doc = Nokogiri::HTML.parse(html, nil, charset)
 
         
-        (4..6).each do |n|
+        (3..15).each do |n|
           if doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[5]/th[1]").inner_text == '住所'
             name = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[5]/td[1]/p[1]").inner_text
-            if name.slice(0, 3) == '鹿児島' or name.slice(0, 3) == '和歌山'
+            if name.slice(0, 3) == '鹿児島' or name.slice(0, 3) == '和歌山' or name.slice(0, 3) == '神奈川'
               name.slice!(0, 4)
             else
               name.slice!(0, 3)
@@ -69,11 +73,25 @@ class BuyHouseCrawlerSuumo
 
             address = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[5]/td[1]/p[1]").inner_text
             buy_house_info.push(name, address)
+          elsif doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[6]/th[1]").inner_text == '住所'
+            name = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[6]/td[1]/p[1]").inner_text
+            if name.slice(0, 3) == '鹿児島' or name.slice(0, 3) == '和歌山' or name.slice(0, 3) == '神奈川'
+              name.slice!(0, 4)
+            else
+              name.slice!(0, 3)
+            end
+
+            address = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[6]/td[1]/p[1]").inner_text
+            buy_house_info.push(name, address)
+            
           else
           end
 
           if doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[5]/th[2]").inner_text == '交通'
             access = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[5]/td[2]/div").inner_text
+            buy_house_info.push(access)
+          elsif doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[6]/th[2]").inner_text == '交通'
+            access = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[6]/td[2]/div").inner_text
             buy_house_info.push(access)
           else
           end
@@ -81,11 +99,18 @@ class BuyHouseCrawlerSuumo
           if doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[1]/th[2]/div[1]").inner_text == '間取り'
             madori = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[1]/td[2]/p").inner_text.sub(/\r.*/m, "")
             buy_house_info.push(madori)
+          elsif doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[2]/th[2]/div[1]").inner_text == '間取り'
+            madori = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[2]/td[2]/p").inner_text.sub(/\r.*/m, "")
+            buy_house_info.push(madori)
           else
           end
 
           if doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[3]/th[1]/div[1]").inner_text == '土地面積'
             land_area = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[3]/td[1]/text()[1]").inner_text.gsub(/\m.*/m, "")
+            land_area.slice!(0, 9)
+            buy_house_info.push(land_area)
+          elsif doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[4]/th[1]/div[1]").inner_text == '土地面積'
+            land_area = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[4]/td[1]/text()[1]").inner_text.gsub(/\m.*/m, "")
             land_area.slice!(0, 9)
             buy_house_info.push(land_area)
           else
@@ -95,12 +120,29 @@ class BuyHouseCrawlerSuumo
             house_area = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[3]/td[2]/text()").inner_text.gsub(/\m.*/m, "")
             house_area.slice!(0, 9)
             buy_house_info.push(house_area)
+          elsif doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[4]/th[2]/div[1]").inner_text == '建物面積'
+            house_area = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[4]/td[2]/text()").inner_text.gsub(/\m.*/m, "")
+            house_area.slice!(0, 9)
+            buy_house_info.push(house_area)
           else
           end
 
           if doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[4]/th[2]/div[1]").inner_text == '完成時期（築年月）'
-            built_date = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[4]/td[2]").inner_text.gsub(/年/m, "-").gsub(/月/m, "").gsub(/\s/m, "")
-            buy_house_info.push(built_date)
+            if doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[4]/td[2]").inner_text.include?("月")
+              built_date = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[4]/td[2]").inner_text.gsub(/年/m, "-").gsub(/月/m, "").gsub(/\s/m, "")
+              buy_house_info.push(built_date)
+            else
+              built_date = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[4]/td[2]").inner_text.gsub(/年/m, "-1").gsub(/\s/m, "")
+              buy_house_info.push(built_date)
+            end
+          elsif doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[5]/th[2]/div[1]").inner_text == '完成時期（築年月）'
+            if doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[5]/td[2]").inner_text.include?("月")
+              built_date = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[5]/td[2]").inner_text.gsub(/年/m, "-").gsub(/月/m, "").gsub(/\s/m, "")
+              buy_house_info.push(built_date)
+            else
+              built_date = doc.xpath("//*[@id='mainContents']/div[#{n}]/div[1]/table[2]/tbody/tr[5]/td[2]").inner_text.gsub(/年/m, "-1").gsub(/\s/m, "")
+              buy_house_info.push(built_date)
+            end
           else
           end
 
@@ -108,7 +150,7 @@ class BuyHouseCrawlerSuumo
         
         strong_point = doc.xpath('//*[@id="mainContents"]/div[2]/h2').inner_text
         prefecture_name = doc.xpath('//*[@id="help_link"]/ul/li[4]/a').inner_text
-        price = doc.xpath('//*[@id="topContents"]/div[2]/div[1]/div[2]/p[1]').inner_text.sub(/万.*/m, "")
+        price = doc.xpath('//*[@id="topContents"]/div[2]/div[1]/div[2]/p[1]').inner_text.sub(/億/m, "").sub(/万.*/m, "")
         zip_code = ''
         hours = ''
         age = ''
@@ -116,11 +158,27 @@ class BuyHouseCrawlerSuumo
         recommendation = 'great'
         shop_id = ''
         source = url
-        image_url1 = doc.xpath('//*[@id="imgG02_001"]').attribute("src").value
-        image_url2 = doc.xpath('//*[@id="imgG02_002"]').attribute("src").value
-        image_url3 = doc.xpath('//*[@id="imgG02_003"]').attribute("src").value
-        image_url4 = doc.xpath('//*[@id="imgG02_004"]').attribute("src").value
-
+        if doc.xpath('//*[@id="imgG02_001"]').present?
+          image_url1 = doc.xpath('//*[@id="imgG02_001"]').attribute("src").value
+        else 
+          image_url1 = ""
+        end
+        if doc.xpath('//*[@id="imgG02_002"]').present?
+          image_url2 = doc.xpath('//*[@id="imgG02_001"]').attribute("src").value
+        else 
+          image_url2 = ""
+        end
+        if doc.xpath('//*[@id="imgG02_003"]').present?
+          image_url3 = doc.xpath('//*[@id="imgG02_001"]').attribute("src").value
+        else 
+          image_url3 = ""
+        end
+        if doc.xpath('//*[@id="imgG02_004"]').present?
+          image_url4 = doc.xpath('//*[@id="imgG02_001"]').attribute("src").value
+        else 
+          image_url4 = ""
+        end
+       
         
         buy_house_info.push(strong_point, prefecture_name, price, zip_code, hours, age, notes, recommendation, shop_id, source, image_url1, image_url2, image_url3, image_url4)
 
