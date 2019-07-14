da1e37634de41b9647dadb902b2fd6c0cb473502
zouazano <sou.asano11@gmail.com>
2019-07-15 00:12:50 +0900

    recheck added to LinkChecker

--- app/services/link_checker.rb
+++ app/services/link_checker.rb
@@ -21,4 +21,21 @@ class LinkChecker
     end
   end
 
+  def self.recheck
+    BuyHouse.where(sold:true).each do |buy_house|
+      unless buy_house.source.nil?
+        url = buy_house.source.gsub(/(.*)(?=http)/,"")
+        
+        unless Net::HTTP.get_response(URI.parse(url)).code == "404" or Net::HTTP.get_response(URI.parse(encoded_url)).code == "403"
+          buy_house.update(sold: false)
+        else
+          buy_house.update(sold: true)
+        end
+        sleep 1
+      else
+        next
+      end
+    end
+  end
+
 end
