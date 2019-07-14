6caa8615897e5f853bdbd65ae60f7b53bdd0a584
zouazano <sou.asano11@gmail.com>
2019-07-15 00:10:59 +0900

    encode deleted from LinkChecker

--- app/services/link_checker.rb
+++ app/services/link_checker.rb
@@ -8,9 +8,8 @@ class LinkChecker
     BuyHouse.all.each do |buy_house|
       unless buy_house.source.nil?
         url = buy_house.source.gsub(/(.*)(?=http)/,"")
-        encoded_url = URI.encode(url)
         
-        unless Net::HTTP.get_response(URI.parse(encoded_url)).code == "404" or Net::HTTP.get_response(URI.parse(encoded_url)).code == "403"
+        unless Net::HTTP.get_response(URI.parse(url)).code == "404" or Net::HTTP.get_response(URI.parse(encoded_url)).code == "403"
           buy_house.update(sold: false)
         else
           buy_house.update(sold: true)
