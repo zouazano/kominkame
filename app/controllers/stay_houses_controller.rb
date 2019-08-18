c31ca45ba089a21c3bb265794124fb5a5d0cddf3
zouazano <sou.asano11@gmail.com>
2019-08-18 09:05:22 +0900

    stay noindex

--- app/controllers/stay_houses_controller.rb
+++ app/controllers/stay_houses_controller.rb
@@ -1,5 +1,6 @@
 class StayHousesController < ApplicationController
   def show
+  	set_meta_tags noindex: true, nofollow: true
     @stay_house = StayHouse.find(params[:id])
     stay_house_id = @stay_house.id
   end
